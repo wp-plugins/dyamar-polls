4079574fe6cd3624b30a23aafd7fa5b0b99414b8
dyamar <dyamar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-30 18:41:34 +0000

    tagging version 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/dyamar-polls/tags/1.1.0@1035559 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/polls.js
@@ -0,0 +1,160 @@
+/*
+
+	DYAMAR Polls - interactive polls for WordPress web sites
+	Copyright (C) 2014  DYAMAR Engineering <info@dyamar.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+function dyamar_polls_send_vote(poll_id, ajaxurl)
+{
+	var has_answer = false;
+	var answer_ids = [];
+
+	jQuery("#dyamar_poll_" + poll_id + " .poll-answers input").each(function(index)
+	{
+		if (jQuery(this).prop('checked'))
+		{
+			has_answer = true;
+			
+			var elem_id = jQuery(this).attr('id');
+			
+			if (elem_id && (elem_id.length > 0))
+			{
+				answer_ids.push(elem_id.replace('answer_', ''));
+			}
+		}
+	});
+	
+	if (!has_answer)
+	{
+		alert('Please specify at least one answer!');
+		
+		return;
+	}
+	
+	var data = {
+		'action': 'dyamar_polls_vote',
+		'poll_id': poll_id,
+		'answer_ids' : answer_ids
+	};
+
+	jQuery.post(ajaxurl, data, function(response)
+	{
+		var result = jQuery.parseJSON(response);
+
+		if (result['status'].indexOf('success') >= 0)
+		{
+			alert('Your vote has been successfully accepted!');
+
+			var answers = result['answers'];
+
+			if (answers instanceof Array)
+			{
+				var index = 0;
+				var total_votes = 0;
+	
+				for (index = 0; index < answers.length; ++index)
+				{
+					total_votes += parseInt(answers[index]['votes']);
+				}
+				
+				for (index = 0; index < answers.length; ++index)
+				{
+					var answer_id = answers[index]['answer_id'];
+					
+					if (answer_id > 0)
+					{
+						var percentage = 0;
+
+						if (total_votes > 0)
+						{
+							percentage = Math.round((parseFloat(answers[index]['votes']) / (total_votes / 100.0)) * 100) / 100;
+						}
+
+						var poll_bar = jQuery("#dyamar_poll_" + poll_id + " #poll_bar_" + answer_id);
+
+						if (poll_bar && (poll_bar.length > 0))
+						{
+							var new_html = '';
+
+							var votes = parseInt(answers[index]['votes']);
+
+							if (votes == 1)
+							{
+								new_html += '<div class="poll-info">' + percentage + '%, ' + votes + ' vote</div>';
+							}
+							else
+							{
+								new_html += '<div class="poll-info">' + percentage + '%, ' + votes + ' votes</div>';
+							}
+
+							if (percentage <= 0)
+							{
+								new_html += '<div class="poll-percentage" style="width:3px;"></div>';
+							}
+							else
+							{
+								new_html += '<div class="poll-percentage" style="width:' + percentage + '%;"></div>';
+							}
+							
+							poll_bar.html(new_html);
+						}
+					}
+				}
+			}
+
+			var lifetime_elem = jQuery("#poll_" + poll_id + "_lifetime");
+
+			if (lifetime_elem && (lifetime_elem.length > 0))
+			{
+				var lifetime = parseInt(lifetime_elem.val());
+
+				if (lifetime > 0)
+				{
+					var currentDate = new Date();
+    				currentDate.setTime(currentDate.getTime() + lifetime * 1000);
+    				var expires = "expires="+currentDate.toGMTString();
+		    		document.cookie = "POLL_" + poll_id + "_VOTED=YES; " + expires;
+		    		
+		    		jQuery("#poll_" + poll_id + "_view_answers").hide();
+
+		    		dyamar_polls_view_result(poll_id);
+		    	}
+		    }
+		}
+		else
+		{
+			alert('Failed to save results. Server is not responding. Please try again later.');
+		}
+	});
+}
+
+function dyamar_polls_view_result(poll_id)
+{
+	jQuery("#dyamar_poll_" + poll_id + " .poll-content").hide();
+	jQuery("#dyamar_poll_" + poll_id + " .poll-result").show();
+
+	return false;
+}
+
+function dyamar_polls_view_answers(poll_id)
+{
+	jQuery("#dyamar_poll_" + poll_id + " .poll-result").hide();
+	jQuery("#dyamar_poll_" + poll_id + " .poll-content").show();
+	
+	return false;
+}
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== DYAMAR Polls ===
+Contributors: dyamar
+Tags: poll, vote, dyamar, feedback, rating, review
+Requires at least: 3.2
+Tested up to: 4.0.1
+Stable tag: 1.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows to add interactive polls to your WordPress web site. You can use shortcuts to place desired questions in a post or in any widget.
+
+== Description ==
+
+This is a plugin that allows to add interactive and dynamic AJAX polls to your WordPress web site. 
+
+You can use generated shortcuts to place desired questions in a post or in any widget.
+
+There are no any limitations and you can create as much polls or questions as you want.
+
+In addition, it's possible to set lifetime of the answer. It means that users will not be able to answer several times and you will see better picture of users' opinions.
+
+== Installation ==
+
+1. Download plugin and unzip.
+2. Upload `dyamar-polls` folder to the `/wp-content/plugins/` directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+4. Go to the `Polls` admin menu in the back-end are of your site.
+5. Create new poll by clicking on the `Add New Poll` button.
+6. Copy short-code and place it inside of any page or widget.
+
+== Frequently Asked Questions ==
+
+= How many polls can I create? =
+
+There are no limitations to the number of polls that you can create in this plugin.
+
+= Does your plugin use AJAX? =
+
+Yes, we do, we use that to send answers to the web site and save it in MySQL database.
+
+= How to insert plugin into page? =
+
+Just copy short-code from the admin section of the Poll plugin and paste it into desired page or widget.
+
+== Changelog ==
+
+= 1.1.0 =
+* Ability to set different color schemes for polls.
+* Validating form data before saving into database.
+* Included CSS files should have correctly specified media type.
+
+= 1.0 =
+* Initial release of this plugin.
